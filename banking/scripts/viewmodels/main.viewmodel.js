ed5641cc44e8b051aa6f28a61b65482c06dc6f97
softcadbury <romain.parage@gmail.com>
2015-08-02 15:20:45 +0200

    Allow the account creation

--- banking/scripts/viewmodels/main.viewmodel.js
+++ banking/scripts/viewmodels/main.viewmodel.js
@@ -1,10 +1,19 @@
 ﻿define('main.viewmodel',
     ['context', 'account.model'],
-    function (context, Account) {
-        context.accounts.push(new Account('Aaa'));
-        context.accounts.push(new Account('Bbb'));
+    function (context, Account) {        
+        var newAccountName = ko.observable('');
+        
+        // Add an account
+        function addAccount() {
+            if (newAccountName().trim()) {
+                context.accounts.push(new Account(newAccountName().trim()));
+                newAccountName('');
+            }
+        }
 
         return {
-        	accounts: context.accounts
+        	accounts: context.accounts,
+            newAccountName: newAccountName,
+            addAccount: addAccount
         };
     });
