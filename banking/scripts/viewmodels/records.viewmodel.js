eda5e8027aea70ad2c05f2a73e80611770d48bf9
Romain Parage <rparage@talentsoft.com>
2015-08-04 13:47:37 +0200

    Add selected accounts to records

--- banking/scripts/viewmodels/records.viewmodel.js
+++ banking/scripts/viewmodels/records.viewmodel.js
@@ -1,8 +1,11 @@
 define('records.viewmodel',
     ['context'],
     function (context) {
+        var selectedAccount = ko.observable();
+        
         return {
             isRecordsViewVisible: context.isRecordsViewVisible,
-            accounts: context.accounts
+            accounts: context.accounts,
+            selectedAccount: selectedAccount
         };
     });
