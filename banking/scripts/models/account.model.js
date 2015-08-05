eaed70b626ff4e3a76e9cc73aba71b6198044fe2
Romain Parage <rparage@talentsoft.com>
2015-08-05 18:21:08 +0200

    Fix account selection

--- banking/scripts/models/account.model.js
+++ banking/scripts/models/account.model.js
@@ -2,9 +2,11 @@ define('account.model',
     function () {
 		return function (accountNname) {
 			var name = ko.observable(accountNname);
+			var isSelected = ko.observable(false);
 	
 	        return {
-	        	name: name
+	        	name: name,
+				isSelected: isSelected
 	        };	
 		};
     });
--- banking/scripts/viewmodels/records.viewmodel.js
+++ banking/scripts/viewmodels/records.viewmodel.js
@@ -3,9 +3,20 @@ define('records.viewmodel',
     function (context) {
         var selectedAccount = ko.observable();
         
+        // Change the selected account
+        function changeSelectedAccount(data) {
+            if (selectedAccount()) {
+                selectedAccount().isSelected(false);    
+            }
+            
+            selectedAccount(data);
+            selectedAccount().isSelected(true);
+        }
+        
         return {
             isRecordsViewVisible: context.isRecordsViewVisible,
             accounts: context.accounts,
-            selectedAccount: selectedAccount
+            selectedAccount: selectedAccount,
+            changeSelectedAccount: changeSelectedAccount
         };
     });
