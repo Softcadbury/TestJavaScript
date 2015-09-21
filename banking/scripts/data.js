ac7b85d4d0dc9c546c92de28758bb48e5e8372b6
softcadbury <romain.parage@gmail.com>
2015-09-21 20:53:57 +0200

    Add file to futur saves and loads of contexts

---
+++ banking/scripts/data.js
@@ -0,0 +1,26 @@
+define('data',
+    ['context'],
+    function (context) {
+        // Loads the context of users
+        function loadUsers() {
+        }
+        
+        // Loads the context of accounts
+        function loadAcounts() {
+        }
+        
+        // Saves the context of users
+        function saveUsers() {
+        }
+        
+        // Saves the context of accounts
+        function saveAccounts() {
+        }
+
+        return {
+            loadUsers: loadUsers,
+            loadAcounts: loadAcounts,
+            saveUsers: saveUsers,
+            saveAccounts: saveAccounts
+        };
+    });
