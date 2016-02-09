4d447761016a0b85e2b4027fa841b8912ed7fcf8
Romain Parage <rparage@talentsoft.com>
2016-02-09 14:22:04 +0100

    Completed gulp injection

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 Some JavaScript projects.
 - Banking: Knockout application to manage users and accounts.
+- Gulp: Tests about Gulp.
+- Node: Tests about Node.
--- node/gulpfile.js
+++ node/gulpfile.js
@@ -6,21 +6,32 @@ var jscs = require('gulp-jscs'); // Check some coding rules
 
 // Check coding rules
 gulp.task('check', function () {
-    gulp.src(['./src/**/*.js', './*.js'])
+    gulp.src(['./public/js/*.js', './*.js'])
         .pipe(jscs())
         .pipe(jscs.reporter())
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
 });
 
+// Inject bower packages in views
 gulp.task('inject', function () {
     var wiredep = require('wiredep').stream;
-    var options = {
+    var inject = require('gulp-inject');
+
+    var wiredepOptions = {
         bowerJson: require('./bower.json'),
-        directory: './public/lib'
+        directory: './public/lib',
+        ignorePath: '../lib'
     };
 
-    return gulp.src('./src/view/*.html')
-        .pipe(wiredep(options))
-        .pipe(gulp.dest('./src/views'));
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], { read: false });
+
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
+    return gulp.src('./public/views/*.html')
+        .pipe(wiredep(wiredepOptions))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./public/views'));
 });
