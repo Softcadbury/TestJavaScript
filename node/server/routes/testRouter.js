d60299e5bfa6abcdaa7b5b44f80dcf34f962c785
softcadbury <romain.parage@gmail.com>
2016-02-14 15:49:59 +0100

    Add route with parameter

--- node/server/routes/testRouter.js
+++ node/server/routes/testRouter.js
@@ -8,6 +8,11 @@ router.route('')
         res.render('test', { title: 'Test' });
     });
 
+router.route('/:id')
+    .get(function (req, res) {
+        res.send(req.params.id);
+    });
+
 router.route('/sub')
     .get(function (req, res) {
         res.send('sub test');
