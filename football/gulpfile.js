0f62d576810d1b9bceb1b1927d8fccb3a14bbba2
softcadbury <romain.parage@gmail.com>
2016-02-21 13:09:11 +0100

    Add goalsFor and goalsAgainst

--- football/gulpfile.js
+++ football/gulpfile.js
@@ -6,38 +6,51 @@ var util = require('util');
 
 gulp.task('read', () => {
     var file = './PL-2014-2015.json';
-    
+
     jsonfile.readFile(file, (err, obj) => {
         var teams = {};
 
         for (var i = 0; i < obj.length; i++) {
             var homeTeam = obj[i].HomeTeam;
-            if (!teams.hasOwnProperty(homeTeam)) {
-                teams[homeTeam] = {
-                    win: 0,
-                    lost: 0,
-                    draw: 0
-                };
-            }
-            
-            teams[homeTeam].win += obj[i].FTR == 'H' ? 1 : 0;
-            teams[homeTeam].lost += obj[i].FTR == 'A' ? 1 : 0;
-            teams[homeTeam].draw += obj[i].FTR == 'D' ? 1 : 0;
-            
             var awayTeam = obj[i].AwayTeam;
-            if (!teams.hasOwnProperty(awayTeam)) {
-                teams[awayTeam] = {
-                    win: 0,
-                    lost: 0,
-                    draw: 0
-                };
+            var homeTeamGoals = obj[i].FTHG;
+            var awayTeamGoals = obj[i].FTAG;
+            var result = obj[i].FTR;
+
+            // Home team
+            if (!teams.hasOwnProperty(homeTeam)) {
+                teams[homeTeam] = createTeamObject();
             }
-            
-            teams[awayTeam].win += obj[i].FTR == 'A' ? 1 : 0;
-            teams[awayTeam].lost += obj[i].FTR == 'H' ? 1 : 0;
-            teams[awayTeam].draw += obj[i].FTR == 'D' ? 1 : 0;
+
+            teams[homeTeam].win += result == 'H' ? 1 : 0;
+            teams[homeTeam].draw += result == 'D' ? 1 : 0;
+            teams[homeTeam].lost += result == 'A' ? 1 : 0;
+            teams[homeTeam].goalsFor += homeTeamGoals;
+            teams[homeTeam].goalsAgainst += awayTeamGoals;
+
+            // Away team
+            if (!teams.hasOwnProperty(awayTeam)) {
+                teams[awayTeam] = createTeamObject();
+            }
+
+            teams[awayTeam].win += result == 'A' ? 1 : 0;
+            teams[awayTeam].draw += result == 'D' ? 1 : 0;
+            teams[awayTeam].lost += result == 'H' ? 1 : 0;
+            teams[awayTeam].goalsFor += awayTeamGoals;
+            teams[awayTeam].goalsAgainst += homeTeamGoals;
         }
 
         console.log(teams);
     })
 });
+
+// Create team object
+function createTeamObject() {
+    return {
+        win: 0,
+        draw: 0,
+        lost: 0,
+        goalsFor: 0,
+        goalsAgainst: 0
+    };
+}
